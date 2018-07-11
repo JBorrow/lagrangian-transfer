95c514efa19b0bb2a04d95030fe8c8095961b592
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-07-10 23:08:04 -0400

    Added module info and setup.py so this can actually be instaleld as a module

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="ltcaesar",
+    version="0.1.0",
+    description="Library for studying transfer between lagrangian regions using Caesar-yt",
+    url="https://github.com/JBorrow/lagrangian-transfer",
+    author="Josh Borrow, Daniel Angles-Alcazar",
+    author_email="joshua.borrow@durham.ac.uk",
+    packages=["ltcaesar"],
+    scripts=["analyse.py"],
+    zip_safe=False
+)
