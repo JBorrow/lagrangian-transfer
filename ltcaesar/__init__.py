095a64cecfc0228ce28a898d1edb49ac7ebb957a
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-08-29 04:10:59 -0400

    Fixed some module issues

--- ltcaesar/__init__.py
+++ ltcaesar/__init__.py
@@ -4,6 +4,6 @@ Lagrangian transfer library that uses the Caesar halo finder.
 
 from .objects import *
 from .inputoutput import write_data_to_file, read_data_from_file
-from .halos import halos
 
 import ltcaesar.analysis as analysis
+import ltcaesar.halos as halos
